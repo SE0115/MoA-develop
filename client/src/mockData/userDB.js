3950ff1b4a7a1bbfa5041eedac31ee4453a0b9ba
sieun <sieun0115@ajou.ac.kr>
2022-07-01 22:44:05 +0900

    mock data 수정

---
+++ client/src/mockData/userDB.js
@@ -0,0 +1,12 @@
+const userDB = [
+  {
+    id: "21-71264703",
+    pw: "12345qwert",
+  },
+  {
+    id: "11-22222222",
+    pw: "11111qqqqq",
+  },
+];
+
+export default userDB;
--- client/src/mockData/usesList.js
+++ client/src/mockData/usesList.js
@@ -1,11 +1,26 @@
 const usersList = [
   {
     id: "21-71264703",
-    join_date: "2021-08-01",
-    key: 48,
-    name: "김민수",
-    phone: "01012345678",
-    unit: "11사단 화랑부대",
+    info: {
+      join_date: "2021-08-01",
+      key: 48,
+      name: "김민수",
+      phone: "01012345678",
+      unit: "11사단 화랑부대",
+      accounts: [],
+    },
   },
-  {},
+  {
+    id: "11-22222222",
+    info: {
+      join_date: "2021-08-01",
+      key: 48,
+      name: "김민수",
+      phone: "01012345678",
+      unit: "11사단 화랑부대",
+      accounts: [],
+    },
+  },
 ];
+
+export default usersList;
