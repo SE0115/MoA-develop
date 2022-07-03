ac0b43d1d7be8b97f51a44230373ee039318077d
sieun <sieun0115@ajou.ac.kr>
2022-07-03 23:35:12 +0900

    reducer - setUser 추가

--- client/src/reducer/userState.js
+++ client/src/reducer/userState.js
@@ -1,18 +1,26 @@
 import createReducer from "../reducer/createReducer";
+import usersList from "mockData/usesList";
 
 const LOGIN = "LOGIN";
 const LOGOUT = "LOGOUT";
-const GETUSER = "GETUSER";
+const SETUSER = "SETUSER";
 
 export const logIn = (userID) => ({ type: LOGIN, userID });
 export const logOut = () => ({ type: LOGOUT });
-export const getUser = (userID) => ({ type: GETUSER, userID });
+export const setUser = (userID) => ({ type: SETUSER, userID });
 
-const INITIAL_STATE = { id: "" };
+const INITIAL_STATE = { id: "", info: [] };
 
 const reducer = createReducer(INITIAL_STATE, {
   [LOGIN]: (state, action) => (state.id = action.userID),
-  [LOGOUT]: (state, action) => (state.id = ""),
+  [LOGOUT]: (state, action) => {
+    state.id = "";
+    state.info = [];
+  },
+  [SETUSER]: (state, action) => {
+    const userIndex = usersList.findIndex((user) => user.id === action.userID);
+    state.info = usersList[userIndex].info;
+  },
 });
 
 export default reducer;
